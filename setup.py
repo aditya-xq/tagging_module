cc62726481e75f30731060ed9f231280724c93b2
aditya-xq <adityavivek1998@gmail.com>
2023-05-26 17:16:52 +0530

    added readme to build

--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@ from setuptools import setup
 
 setup(
     name='tagging_module',
-    version='0.0.6',
+    version='0.0.7',
     author='aditya-xq',
     author_email='adityavivek.xq@gmail.com',
     description='A lightweight rule based tech content tagging module for Python',
-    packages=['tagging_module'],
+    packages=['tagging_module', 'tagging_module.config'],
     package_data={
         'tagging_module': ['config/rules.yml'],
     },
     include_package_data=True,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     install_requires=[
         'PyYAML',
     ],
